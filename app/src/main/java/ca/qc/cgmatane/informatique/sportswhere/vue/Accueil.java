c81dec7141286a160cc4dca7978e7a1fe2e678a4
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-10-01 18:51:10 -0400

    Ajout Hardcodage terrains sur map (tests)

--- app/src/main/java/ca/qc/cgmatane/informatique/sportswhere/vue/Accueil.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/sportswhere/vue/Accueil.java
@@ -1,14 +1,25 @@
 package ca.qc.cgmatane.informatique.sportswhere.vue;
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.Typeface;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
 
 import ca.qc.cgmatane.informatique.sportswhere.R;
 
@@ -40,9 +51,81 @@ public class Accueil extends FragmentActivity implements OnMapReadyCallback {
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
 
-        // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+
+        LatLng matane = new LatLng( 48.8526, -67.518);
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(matane));
+        mMap.animateCamera(CameraUpdateFactory.zoomTo(13), 2000, null);
+
+
+        LatLng terrainDeLaPoly = new LatLng(48.839393,-67.504567);
+        mMap.addMarker(new MarkerOptions().position(terrainDeLaPoly).title("Terrain de la Polyvalente de Matane").snippet("1 terrain en vrai gazon").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_RED)));
+        PolygonOptions terrainPolyOption = new PolygonOptions()
+                .add(new LatLng(48.839725, -67.505143),
+                        new LatLng(48.839283, -67.505416),
+                        new LatLng(48.838959, -67.504174),
+                        new LatLng(48.839431, -67.503941));
+
+        Polygon footPoly = mMap.addPolygon(terrainPolyOption.fillColor(Color.GREEN));
+        LatLng terrainDuCegep = new LatLng(48.840856, -67.495824);
+        mMap.addMarker(new MarkerOptions().position(terrainDuCegep).title("Terrain du CEGEP de Matane").snippet("2 terrains en vrai gazon"+"\n"+"1 terrain synthétique").icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN)));
+        PolygonOptions terrainCegepOption1 = new PolygonOptions()
+                .add(new LatLng(48.842417,  -67.494738),
+                        new LatLng(48.841966, -67.495110),
+                        new LatLng(48.841531, -67.493940),
+                        new LatLng(48.841997, -67.493552));
+
+        Polygon footCegep1 = mMap.addPolygon(terrainCegepOption1.fillColor(Color.GREEN));
+        footCegep1.setClickable(true);
+
+        PolygonOptions terrainCegepOption2 = new PolygonOptions()
+                .add(new LatLng(48.840203,   -67.496060),
+                        new LatLng(48.840442, -67.495909),
+                        new LatLng(48.840192,   -67.494790),
+                        new LatLng(48.839886, -67.494838));
+
+        Polygon footCegep2 = mMap.addPolygon(terrainCegepOption2.fillColor(Color.GREEN));
+        footCegep2.setClickable(true);
+        PolygonOptions terrainCegepOption3 = new PolygonOptions()
+                .add(new LatLng(48.841361,  -67.494814),
+                        new LatLng(48.841142, -67.494961),
+                        new LatLng(48.840964,  -67.494275),
+                        new LatLng(48.841181, -67.494153));
+
+        Polygon footCegep3 = mMap.addPolygon(terrainCegepOption3.fillColor(Color.BLUE));
+        footCegep3.setClickable(true);
+
+
+        mMap.setInfoWindowAdapter(new GoogleMap.InfoWindowAdapter() {
+
+            @Override
+            public View getInfoWindow(Marker arg0) {
+                return null;
+            }
+            @Override
+            public View getInfoContents(Marker marker) {
+
+                Context mContext = getApplicationContext();
+                LinearLayout info = new LinearLayout(mContext);
+                info.setOrientation(LinearLayout.VERTICAL);
+
+                TextView title = new TextView(mContext);
+                title.setTextColor(Color.BLACK);
+                title.setGravity(Gravity.CENTER);
+                title.setTypeface(null, Typeface.BOLD);
+                title.setText(marker.getTitle());
+
+                TextView snippet = new TextView(mContext);
+                snippet.setTextColor(Color.GRAY);
+                snippet.setText(marker.getSnippet());
+
+                info.addView(title);
+                info.addView(snippet);
+
+                return info;
+            }
+        });
+
+
+
     }
 }
