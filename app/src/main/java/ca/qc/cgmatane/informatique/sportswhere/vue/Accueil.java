bc37e9a4150c1244bda3b9f4b003dd96fb2b72b7
Vincent Seyller <31846859+VinSey1@users.noreply.github.com>
2018-10-01 23:05:29 -0400

    Navigation entre Accueil et Liste Evenements et Liste Terrains

--- app/src/main/java/ca/qc/cgmatane/informatique/sportswhere/vue/Accueil.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/sportswhere/vue/Accueil.java
@@ -1,12 +1,14 @@
 package ca.qc.cgmatane.informatique.sportswhere.vue;
 
 import android.content.Context;
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.Typeface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
@@ -29,23 +31,53 @@ import ca.qc.cgmatane.informatique.sportswhere.modele.Terrain;
 
 public class Accueil extends AppCompatActivity implements OnMapReadyCallback {
 
+    static final public int ACTIVITE_LISTE_TERRAINS = 1;
+    static final public int ACTIVITE_LISTE_EVENEMENTS = 2;
+
     private GoogleMap carteTerrains;
     private TerrainDAO accesseurTerrain;
     private EvenementDAO accesseurEvenement;
     private List<Terrain> listeTerrains;
+    private Intent intentionNaviguerListeTerrains;
+    private Intent intentionNaviguerListeEvenements;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.vue_accueil);
 
-        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.map);
+        MapFragment mapFragment = (MapFragment) getFragmentManager().findFragmentById(R.id.carte_terrains);
         mapFragment.getMapAsync(this);
 
         accesseurEvenement = EvenementDAO.getInstance();
         accesseurTerrain = TerrainDAO.getInstance();
 
         listeTerrains = accesseurTerrain.listerTerrains();
+
+        intentionNaviguerListeTerrains = new Intent(this, ListeTerrains.class);
+
+        Button actionNaviguerListeTerrains = (Button) findViewById(R.id.action_naviguer_liste_terrains);
+
+        actionNaviguerListeTerrains.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View arg0) {
+                    startActivityForResult(intentionNaviguerListeTerrains, ACTIVITE_LISTE_TERRAINS);
+                }
+            }
+        );
+
+        intentionNaviguerListeEvenements = new Intent(this, ListeEvenements.class);
+
+        Button actionNaviguerListeEvenements = (Button) findViewById(R.id.action_naviguer_liste_evenements);
+
+        actionNaviguerListeEvenements.setOnClickListener(
+            new View.OnClickListener() {
+                public void onClick(View arg0) {
+                    startActivityForResult(intentionNaviguerListeEvenements, ACTIVITE_LISTE_EVENEMENTS);
+                }
+            }
+        );
+
     }
 
     @Override
