8cbca45d5a2acac6ab7d25518135f1da64842939
Vincent Seyller <vseyller@laposte.net>
2018-10-21 17:45:38 -0400

    Récupération des Terrains depuis XML BD

--- app/src/main/java/ca/qc/cgmatane/informatique/sportswhere/donnee/TerrainDAO.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/sportswhere/donnee/TerrainDAO.java
@@ -1,18 +1,36 @@
 package ca.qc.cgmatane.informatique.sportswhere.donnee;
 
 import android.database.Cursor;
+import android.util.Log;
 
 import com.google.android.gms.maps.model.LatLng;
 
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.xml.sax.SAXException;
+
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+import java.io.StringBufferInputStream;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
+import java.util.concurrent.ExecutionException;
+
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
 
 import ca.qc.cgmatane.informatique.sportswhere.modele.Terrain;
+
+import static java.lang.Thread.sleep;
 
 public class TerrainDAO {
 
     private static TerrainDAO instance = null;
+    private ServiceDAO accesseurService;
+    private String xml;
 
     protected List<Terrain> listeTerrains;
 
@@ -24,12 +42,51 @@ public class TerrainDAO {
     }
 
     public TerrainDAO(){
-        initialiserDonneesTestTerrains();
+        try {
+            accesseurService = new ServiceDAO();
+            xml = accesseurService.execute("http://158.69.192.249/sportswhere/liste_terrain.php", "</terrains>").get();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+        try {
+            initialiserDonneesTestTerrains();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (SAXException e) {
+            e.printStackTrace();
+        } catch (ParserConfigurationException e) {
+            e.printStackTrace();
+        }
     }
 
-    private void initialiserDonneesTestTerrains(){
-
+    private void initialiserDonneesTestTerrains() throws IOException, SAXException, ParserConfigurationException {
         listeTerrains = new ArrayList<Terrain>();
+        DocumentBuilder parseur = DocumentBuilderFactory.newInstance().newDocumentBuilder();
+        Document document = parseur.parse(new ByteArrayInputStream(xml.getBytes("UTF-8")));
+        NodeList listeNoeudAnnonces = document.getElementsByTagName("terrain");
+
+        for (int iterateur = 0; iterateur < listeNoeudAnnonces.getLength(); iterateur++) {
+            Element noeudAnnonce = (Element) listeNoeudAnnonces.item(iterateur);
+
+            int id = Integer.parseInt(noeudAnnonce.getElementsByTagName("id_terrain").item(0).getTextContent());
+            String nom = noeudAnnonce.getElementsByTagName("nom").item(0).getTextContent();
+            String ville = noeudAnnonce.getElementsByTagName("ville").item(0).getTextContent();
+            String description = noeudAnnonce.getElementsByTagName("description").item(0).getTextContent();
+            String images = noeudAnnonce.getElementsByTagName("images").item(0).getTextContent();
+            Float latitude = Float.parseFloat(noeudAnnonce.getElementsByTagName("latitude").item(0).getTextContent());
+            Float longitude = Float.parseFloat(noeudAnnonce.getElementsByTagName("longitude").item(0).getTextContent());
+
+            LatLng position = new LatLng(latitude, longitude);
+
+            Terrain terrain = new Terrain(position, nom, description, ville, id);
+
+            listeTerrains.add(terrain);
+        }
+
+        /*
 
         LatLng position = new LatLng( 48.840897, -67.50821);
         String titre = "Terrain de la Polyvalente de Matane";
@@ -43,7 +100,7 @@ public class TerrainDAO {
         description = "2 terrains en vrai gazon\n1 terrain synthétique";
         terrain = new Terrain(position, titre, description, ville,1);
         listeTerrains.add(terrain);
-
+        */
     }
 
     public Terrain trouverTerrain(String nom){
