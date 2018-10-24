0ac55709ec2f23619fed84332e1ad5289e5d852b
Vincent Seyller <vseyller@laposte.net>
2018-10-23 22:31:46 -0400

    Clarification et correction synthaxe Alarme

--- app/src/main/java/ca/qc/cgmatane/informatique/sportswhere/vue/Alarme.java
+++ app/src/main/java/ca/qc/cgmatane/informatique/sportswhere/vue/Alarme.java
@@ -1,11 +1,8 @@
 package ca.qc.cgmatane.informatique.sportswhere.vue;
 
 import android.app.AlarmManager;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Intent;
-import android.os.Build;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -15,8 +12,6 @@ import java.text.SimpleDateFormat;
 
 import ca.qc.cgmatane.informatique.sportswhere.R;
 import ca.qc.cgmatane.informatique.sportswhere.modele.AlarmeReception;
-
-import static android.support.v4.content.ContextCompat.getSystemService;
 
 public class Alarme extends AppCompatActivity {
 
@@ -37,9 +32,7 @@ public class Alarme extends AppCompatActivity {
         PendingIntent attenteIntentionLancerAlarme = PendingIntent.getBroadcast(this, 0, intententionLancerAlarme, 0);
 
         AlarmManager gestionnaireAlarme = (AlarmManager) getSystemService(ALARM_SERVICE);
-
-        Log.d("TestTemps",""+System.currentTimeMillis());
-
+        
         gestionnaireAlarme.set(AlarmManager.RTC_WAKEUP, tempsAlarme, attenteIntentionLancerAlarme);
 
         Toast.makeText(this, "Alarme prévue pour le " + new SimpleDateFormat("dd/MM/yyyy").format(tempsAlarme),Toast.LENGTH_LONG).show();
